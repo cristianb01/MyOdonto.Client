78376bba23d1c6a17c32c0424ba8a80e337b5fe1
Cristian Bedoya <cristianbedoyavargas@gmail.com>
2023-03-08 11:36:49 -0500

    sdf

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://myodontoapiapi.azure-api.net/api',
+    apiUrl: 'https://localhost:7238/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:4200',
+    apiUrl: 'https://myodontoapiapi.azure-api.net/api',
 };
