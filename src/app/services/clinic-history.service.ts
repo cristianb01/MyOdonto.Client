ab47455a5e8930b550bbfb61d18afe25ef62a995
Cristian Bedoya <cristianbedoyavargas@gmail.com>
2023-03-08 11:35:42 -0500

    enviroment variable imported correctly

--- src/app/services/clinic-history.service.ts
+++ src/app/services/clinic-history.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ClinicHistoryRequestModel, ClinicHistoryResponseModel } from '../models/clinic-history.model';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 import { lastValueFrom } from 'rxjs';
 
 @Injectable({
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7238/api',
+    apiUrl: 'https://myodontoapiapi.azure-api.net/api',
 };
